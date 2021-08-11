0809bfb570908d2850e1bda6c22805d85f93e04f
enzocalderonn <83834531+enzocalderonn@users.noreply.github.com>
2021-08-11 12:33:11 -0400

    version 6 11-08-2021

--- server.js
+++ server.js
@@ -7,18 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/client'));
 
-app.set('/', (req, res) =>
+app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/client/'}),
 );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-
-
-
-
-
-
-
-
