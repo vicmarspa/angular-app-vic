b42876b8ccebf1678df0c1c1e5351888ae277cfd
enzocalderonn <83834531+enzocalderonn@users.noreply.github.com>
2022-04-17 17:23:58 -0400

    v1 17/04/2022

--- src/app/services/servicio-camiones.service.ts
+++ src/app/services/servicio-camiones.service.ts
@@ -12,8 +12,8 @@ import { Observable, BehaviorSubject } from 'rxjs';
 })
 export class ServicioCamionesService {
 
-      // API_URI = 'https://vicmarspa.herokuapp.com/';
-      API_URI = 'https://vicmarspa.herokuapp.com/';
+      // API_URI = 'http://localhost:3000/';
+      API_URI = 'http://localhost:3000/';
 
       authSubject = new BehaviorSubject(false);
       private token: string;
